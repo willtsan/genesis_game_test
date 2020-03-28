6b3503f868c60658ad4c96e48e7bd6ad1d595248
William Santos <william.santos@restoque.com.br>
2020-03-28 02:13:33 -0300

    Megarunner v1

--- MegaRunner/res/resource.h
+++ MegaRunner/res/resource.h
@@ -2,6 +2,7 @@
 #define _RES_RESOURCE_H_
 
 extern const SpriteDefinition runner;
+extern const SpriteDefinition rock;
 extern const Image floor;
 extern const Image wall;
 extern const Image light;
--- MegaRunner/src/main.c
+++ MegaRunner/src/main.c
@@ -11,9 +11,31 @@ char msg_reset[22] = "Press START to Reset!";
 const int scrollspeed = 2;
 bool game_on = FALSE;
 
+/*Obstacle stuff*/
+Sprite* obstacle;
+int obstacle_x = 320;
+int obstacle_vel_x = 0;
+
 Sprite* player;
 const int player_x = 32;
 int player_y = 112;
+fix16 player_vel_y = FIX16(112);
+
+int player_height = 16;
+const int floor_height = 128;
+fix16 gravity = FIX16(0.2);
+bool jumping = TRUE;
+
+int score = 0;
+char label_score[6] = "SCORE\0";
+char str_score[3] = "0";
+bool score_added = FALSE;
+
+void updateScoreDisplay(){
+	sprintf(str_score,"%d",score);
+	VDP_clearText(1,2,3);
+	VDP_drawText(str_score,10,2);
+}
 
 void showText(char s[]){
 	VDP_drawText(s, 20 - strlen(s)/2 ,10);
@@ -27,6 +49,11 @@ void startGame(){
 	if(game_on == FALSE){
 		game_on = TRUE;
 		clearText();
+
+		VDP_drawText(label_score,10,1);
+		score = 0;
+		updateScoreDisplay();
+		obstacle_x = 320;
 	}
 }
 
@@ -34,6 +61,7 @@ void endGame(){
 	if(game_on == TRUE){
 		showText(msg_reset);
 		game_on = FALSE;
+
 	}
 }
 
@@ -50,7 +78,11 @@ void myJoyHandler( u16 joy, u16 changed, u16 state)
 		}
 		if (state & BUTTON_C)
 		{
-			
+			if(jumping == FALSE){
+				jumping = TRUE;
+				player_vel_y = FIX16(-4);
+				SPR_setAnim(player,ANIM_JUMP);
+			}
 		}
 	}
 }
@@ -74,6 +106,7 @@ int main()
 	VDP_loadTileSet(light.tileset,3,DMA);
 
 	player = SPR_addSprite(&runner,player_x,player_y,TILE_ATTR(PAL2,0,FALSE,FALSE));
+	obstacle = SPR_addSprite(&rock,obstacle_x,128,TILE_ATTR(PAL2,0,FALSE,FALSE));
 	SPR_setAnim(player,ANIM_RUN);
 
 	VDP_setPalette(PAL1, light.palette->data);
@@ -93,8 +126,48 @@ int main()
 
 		if(offset <= -256) offset = 0;
 
+		//Move the obstacle
+		obstacle_vel_x = -scrollspeed;
+		obstacle_x = obstacle_x + obstacle_vel_x;
+		if(obstacle_x < -8) obstacle_x = 320;
+
 		if(game_on == TRUE){
+
+			//Apply velocity
+			player_y = fix16Add(player_y, player_vel_y);
+
+			//Apply gravity
+			if(jumping == TRUE) player_vel_y = fix16Add(player_vel_y, gravity);
+
+			//Check if player is on floor
+			if(jumping == TRUE && fix16ToInt(player_y)+player_height >= (floor_height)){
+				jumping = FALSE;
+				score_added = FALSE;
+				player_vel_y = FIX16(0);
+				player_y = intToFix16(floor_height-player_height);
+				SPR_setAnim(player,ANIM_RUN);
+			}
+			
+			SPR_setPosition(obstacle,obstacle_x,120);
+
+			if(player_x < obstacle_x+8 && player_x + 8 > obstacle_x){
+				if(jumping == FALSE){
+					endGame();
+				} else {
+					if(score_added == FALSE){
+						score++;
+						updateScoreDisplay();
+						score_added = TRUE;
+					}
+				}		
+			}
+
+			SPR_setPosition(player,player_x,fix16ToInt(player_y));
+			
 			SPR_update();
+		} else {
+			//clearText();
+			SPR_clear();
 		}
 		VDP_waitVSync();
 	}
